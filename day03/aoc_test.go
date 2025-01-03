8106490c7f7ed637cd19cf162525f2715ca30e56
¨Sebastien <sebastien.mariaux@pm.me>
2025-01-03 18:12:12 +0100

    day 03 part 2

--- day03/aoc_test.go
+++ day03/aoc_test.go
@@ -4,32 +4,32 @@ import "testing"
 
 func TestSampleDataPart1(t *testing.T) {
 	expected := 161
-	result := part1(true)
+	result := solvePart1(("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"))
 	if result != expected {
 		t.Errorf("Failed - Expected %d, Got %d", expected, result)
 	}
 }
 
 func TestDataPart1(t *testing.T) {
-	expected := 0
+	expected := 182619815
 	result := part1(false)
 	if result != expected {
 		t.Errorf("Failed - Expected %d, Got %d", expected, result)
 	}
 }
 
-// func TestSampleDataPart2(t *testing.T) {
-// 	expected := 0
-// 	result := part2(true)
-// 	if result != expected {
-// 		t.Errorf("Failed - Expected %d, Got %d", expected, result)
-// 	}
-// }
+func TestSampleDataPart2(t *testing.T) {
+	expected := 48
+	result := solvePart2("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
+	if result != expected {
+		t.Errorf("Failed - Expected %d, Got %d", expected, result)
+	}
+}
 
-// func TestDataPart2(t *testing.T) {
-// 	expected := 0
-// 	result := part2(false)
-// 	if result != expected {
-// 		t.Errorf("Failed - Expected %d, Got %d", expected, result)
-// 	}
-// }
+func TestDataPart2(t *testing.T) {
+	expected := 80747545
+	result := part2(false)
+	if result != expected {
+		t.Errorf("Failed - Expected %d, Got %d", expected, result)
+	}
+}
--- day03/main.go
+++ day03/main.go
@@ -6,11 +6,12 @@ import (
 	"strconv"
 )
 
-
-
 func part1(sample bool) int {
 	data := helpers.OpenInput(sample)
+	return solvePart1(data)
+}
 
+func solvePart1(data string) int {
 	regex := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
 
 	// find all matches
@@ -22,11 +23,36 @@ func part1(sample bool) int {
 		count += firstVal * secondVal
 	}
 
+	return count
+}
+
+func solvePart2(data string) int {
+	regex := regexp.MustCompile(`mul\((\d+),(\d+)\)|(don't\(\))|(do\(\))`)
+
+	// find all matches
+	matches := regex.FindAllStringSubmatch(data, -1)
+	count := 0
+	canMultiply := true
+	for _, match := range matches {
+		if match[0] == "don't()" {
+			canMultiply = false
+			continue
+		}
+		if match[0] == "do()" {
+			canMultiply = true
+			continue
+		}
+		if canMultiply {
+			firstVal, _ := strconv.Atoi(match[1])
+			secondVal, _ := strconv.Atoi(match[2])
+			count += firstVal * secondVal
+		}
+	}
 
 	return count
 }
 
 func part2(sample bool) int {
-	// data := helpers.OpenInput(sample)
-	return 0
+	data := helpers.OpenInput(sample)
+	return solvePart2(data)
 }
